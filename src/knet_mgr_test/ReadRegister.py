62f6ee2d218a4af2006926e6d8cc819134155177
xinshengzzy <xinshengzzy@gmail.com>
2019-11-28 13:05:00 +0000

    Automatic upload. No comment.

--- src/knet_mgr_test/ReadRegister.py
+++ src/knet_mgr_test/ReadRegister.py
@@ -5,8 +5,8 @@ from thrift.transport import TTransport
 import conn_mgr_pd_rpc.conn_mgr
 from res_pd_rpc.ttypes import *
 from ptf.thriftutils import *
-from AHashFlow.p4_pd_rpc.ttypes import *
-import AHashFlow.p4_pd_rpc.AHashFlow as AHashFlow
+from knet_mgr_test.p4_pd_rpc.ttypes import *
+import knet_mgr_test.p4_pd_rpc.knet_mgr_test as knet_mgr_test
 
 thrift_server = "localhost"
 #transport = TSocket.TSocket(thrift_server, 9090)
@@ -20,12 +20,12 @@ sess_hdl = conn_mgr.client_init()
 dev = 0
 dev_tgt = DevTarget_t(dev, hex_to_i16(0xFFFF))
 
-p4_prefix = "AHashFlow"
+p4_prefix = "knet_mgr_test"
 p4_protocol = TMultiplexedProtocol.TMultiplexedProtocol(bprotocol, p4_prefix) 
-client = AHashFlow.Client(p4_protocol)
-flag = AHashFlow_register_flags_t(read_hw_sync = True)
+client = knet_mgr_test.Client(p4_protocol)
+flag = knet_mgr_test_register_flags_t(read_hw_sync = True)
 #res = client.register_read_pktcnt(sess_hdl, dev_tgt, 0, flag)
-client.register_write_main_table_1_value(sess_hdl, dev_tgt, 0, 5)
-res = client.register_read_main_table_1_value(sess_hdl, dev_tgt, 0, flag)
+#client.register_write_main_table_1_value(sess_hdl, dev_tgt, 0, 5)
+res = client.register_read_cntr(sess_hdl, dev_tgt, 0, flag)
 print "res:", res
 conn_mgr.client_cleanup(hex_to_i32(sess_hdl))
--- src/meters/run.sh
+++
@@ -1,1 +0,0 @@
-/root/bf-sde-8.2.0/run_switchd.sh -p meters
